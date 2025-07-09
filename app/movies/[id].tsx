7ab53c8d09a77dce7fe8d69ccaf7d7a99fa89f45
Tomohiro-Akinaga <akitomo129@gmail.com>
2025-07-09 18:12:43 +0800

    fix: warningの解消

---
+++ app/movies/[id].tsx
@@ -0,0 +1,11 @@
+import { useLocalSearchParams } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function MovieDetail() {
+  const { id } = useLocalSearchParams();
+  return (
+    <View>
+      <Text>Movie details : {id}</Text>
+    </View>
+  );
+}
